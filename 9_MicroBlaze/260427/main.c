70b7a5bb3aa21bc7d81a51be4763d724496fd801
unknown <sumin1201@ajou.ac.kr>
2026-04-28 12:22:16 +0900

    complete up counter

--- 9_MicroBlaze/260427/main.c
+++ 9_MicroBlaze/260427/main.c
@@ -5,14 +5,38 @@
 #include "HAL/GPIO/GPIO.h"
 #include "driver/FND/FND.h"
 
+uint32_t millis_tick = 0;
+
+uint32_t millis(){
+	return millis_tick;
+}
+void millis_inc(){
+	millis_tick++;
+}
+void delay_ms(uint32_t msec){
+	usleep(msec*1000);
+}
+void delay_us(uint32_t usec){
+	usleep(usec);
+}
+
+
 int main(){
 
 	FND_Init();
-	FND_SetNum(1234);
+
+	uint16_t counter = 0;
+	uint32_t prevTimeCounter = 0;
 
 	while(1){
+		if(millis() - prevTimeCounter >= 100) {
+			prevTimeCounter = millis();
+			FND_SetNum(counter++);
+		}
+
 		FND_DispDigit();
-		usleep(1000);
+		millis_inc();
+		delay_ms(1);
 	}
 
 	return 0;
