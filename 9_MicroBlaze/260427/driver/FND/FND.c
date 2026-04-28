dfd891138cf4d0222eb52567bc59ab765db68afa
unknown <sumin1201@ajou.ac.kr>
2026-04-28 12:17:42 +0900

    complete driver/FND

---
+++ 9_MicroBlaze/260427/driver/FND/FND.c
@@ -0,0 +1,85 @@
+/*
+ * FND.c
+ *
+ *  Created on: 2026. 4. 28.
+ *      Author: kccistc
+ */
+
+#include "FND.h"
+
+uint8_t fndNumData = 0;
+
+void FND_Init(){
+	// GPIO 설정, GPIOA 0, 1, 2, 3 COM(common port) 연결
+	GPIO_SetMode(FND_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, OUTPUT);
+	// GPIO 설정, GPIOB seg a~f, dp 연결
+	GPIO_SetMode(FND_FONT_PORT, SEG_PIN_A|SEG_PIN_B|SEG_PIN_C|SEG_PIN_D|SEG_PIN_E|SEG_PIN_F|SEG_PIN_G|SEG_PIN_DP, OUTPUT);
+}
+void FND_SetComPort(GPIO_Typedef_t *GPIOx, uint32_t Seg_Pin, int OnOFF){
+	GPIO_WritePin(FND_COM_PORT, Seg_Pin, OnOFF);
+}
+void FND_DispDigit(){
+	static uint8_t fndDigState = 0;
+	fndDigState = (fndDigState + 1) % 4;
+
+	switch(fndDigState){
+		case 0:
+			FND_DispDigit_1();
+			break;
+		case 1:
+			FND_DispDigit_10();
+			break;
+		case 2:
+			FND_DispDigit_100();
+			break;
+		case 3:
+			FND_DispDigit_1000();
+			break;
+		default: break;
+	}
+}
+void FND_DispDigit_1(){
+	uint8_t fndFont[16] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90, 0x88, 0x83, 0xc6, 0xa1, 0x86, 0x8e};
+
+	// data의 자릿수 분리
+	uint8_t digitData1 = fndNumData % 10;
+	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, OFF);
+	GPIO_WritePort(FND_FONT_PORT, fndFont[digitData1]);
+	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_1, ON);
+}
+void FND_DispDigit_10(){
+	uint8_t fndFont[16] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90, 0x88, 0x83, 0xc6, 0xa1, 0x86, 0x8e};
+
+	// data의 자릿수 분리
+	uint8_t digitData10 = fndNumData / 10 % 10;
+	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, OFF);
+	GPIO_WritePort(FND_FONT_PORT, fndFont[digitData10]);
+	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_2, ON);
+}
+void FND_DispDigit_100(){
+	uint8_t fndFont[16] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90, 0x88, 0x83, 0xc6, 0xa1, 0x86, 0x8e};
+
+	// data의 자릿수 분리
+	uint8_t digitData100 = fndNumData / 100 % 10;
+	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, OFF);
+	GPIO_WritePort(FND_FONT_PORT, fndFont[digitData100]);
+	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_3, ON);
+}
+void FND_DispDigit_1000(){
+	uint8_t fndFont[16] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90, 0x88, 0x83, 0xc6, 0xa1, 0x86, 0x8e};
+
+	// data의 자릿수 분리
+	uint8_t digitData1000 = fndNumData / 1000 % 10;
+	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, OFF);
+	GPIO_WritePort(FND_FONT_PORT, fndFont[digitData1000]);
+	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_4, ON);
+}
+void FND_SetNum(uint16_t num){
+	fndNumData = num;
+}
+void FND_DispOn(){
+	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, ON);
+}
+void FND_DispOFF(){
+	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, OFF);
+}
--- 9_MicroBlaze/260427/main.c
+++ 9_MicroBlaze/260427/main.c
@@ -3,129 +3,16 @@
 #include "sleep.h"
 #include "xil_printf.h"
 #include "HAL/GPIO/GPIO.h"
-
-#define FND_FONT_PORT	GPIOB
-#define FND_COM_PORT 	GPIOA
-
-#define FND_COM_DIG_1	GPIO_PIN_0
-#define FND_COM_DIG_2	GPIO_PIN_1
-#define FND_COM_DIG_3	GPIO_PIN_2
-#define FND_COM_DIG_4	GPIO_PIN_3
-
-#define SEG_PIN_A	GPIO_PIN_0
-#define SEG_PIN_B	GPIO_PIN_1
-#define SEG_PIN_C	GPIO_PIN_2
-#define SEG_PIN_D	GPIO_PIN_3
-#define SEG_PIN_E	GPIO_PIN_4
-#define SEG_PIN_F	GPIO_PIN_5
-#define SEG_PIN_G	GPIO_PIN_6
-#define SEG_PIN_DP	GPIO_PIN_7
-
-#define ON	0
-#define OFF	1
-
-uint8_t fndNumData = 0;
-
-void FND_Init(){
-	// GPIO 설정, GPIOA 0, 1, 2, 3 COM(common port) 연결
-	GPIO_SetMode(GPIO_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, OUTPUT);
-	// GPIO 설정, GPIOB seg a~f, dp 연결
-	GPIO_SetMode(FND_FONT_PORT, SEG_PIN_A|SEG_PIN_B|SEG_PIN_C|SEG_PIN_D|SEG_PIN_E|SEG_PIN_F|SEG_PIN_G|SEG_PIN_DP, OUTPUT);
-}
-void FND_SetComPort(GPIO_Typedef_t *GPIOx, uint32_t Seg_Pin, int OnOFF){
-	GPIO_WritePin(FND_Port, Seg_Pin, OnOFF);
-}
-void FND_DispDigit(){
-	static uint8_t fndDigState = 0;
-	fndDigState = (fndDigState + 1) % 4;
-
-	switch(fndDigState){
-		case 0: 
-			FND_DispDgit1();
-			break;
-		case 0: 
-			FND_DispDgit10();
-			break;
-		case 0: 
-			FND_DispDgit100();
-			break;
-		case 0: 
-			FND_DispDgit1000();
-			break;
-		default: break;
-	}
-}
-void FND_Dispdigit_1(){
-	uint8_t fndFont[16] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90, 0x88, 0x83, 0xc6, 0xa1, 0x86, 0x8e};
-
-	// data의 자릿수 분리
-	uint8_t digitData1 = fndNumData % 10;
-	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, OFF);
-	GPIO_WritePort(FND_FONT_PORT, fndFont[digitData1]);
-	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_1, ON);
-}
-void FND_DispDigit_10(){
-	uint8_t fndFont[16] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90, 0x88, 0x83, 0xc6, 0xa1, 0x86, 0x8e};
-
-	// data의 자릿수 분리
-	uint8_t digitData1 = fndNumData / 10 % 10;
-	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, OFF);
-	GPIO_WritePort(FND_FONT_PORT, fndFont[digitData10]);
-	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_2, ON);
-}
-void FND_DispDigit_100(){
-	uint8_t fndFont[16] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90, 0x88, 0x83, 0xc6, 0xa1, 0x86, 0x8e};
-
-	// data의 자릿수 분리
-	uint8_t digitData1 = fndNumData / 100 % 10;
-	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, OFF);
-	GPIO_WritePort(FND_FONT_PORT, fndFont[digitData100]);
-	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_3, ON);
-}
-void FND_DispDigit_1000(){
-	uint8_t fndFont[16] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xf8, 0x80, 0x90, 0x88, 0x83, 0xc6, 0xa1, 0x86, 0x8e};
-
-	// data의 자릿수 분리
-	uint8_t digitData1 = fndNumData / 1000 % 10;
-	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, OFF);
-	GPIO_WritePort(FND_FONT_PORT, fndFont[digitData1000]);
-	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_4, ON);
-}
-void FND_SetNum(uint16 num){
-	fndNumData = num;
-}
-void FND_DispOn(){
-	GPIO_WritePort(FND_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, ON);
-}
-void FND_DispOFF(){
-	FND_SetComPort(FND_COM_PORT, FND_COM_DIG_1|FND_COM_DIG_2|FND_COM_DIG_3|FND_COM_DIG_4, OFF);
-}
-
+#include "driver/FND/FND.h"
 
 int main(){
 
-	GPIO_SetMode(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, OUTPUT);
-	GPIO_SetMode(GPIOA, GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, INPUT);
-	GPIO_SetMode(GPIOB, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, OUTPUT);
+	FND_Init();
+	FND_SetNum(1234);
 
 	while(1){
-		GPIO_WritePort(GPIOB, 0x00);
-		
-		if(GPIO_ReadPin(GPIOA, GPIO_PIN_4)){
-			GPIO_WritePin(GPIOA, GPIO_PIN_0, RESET);
-		}
-		else if(GPIO_ReadPin(GPIOA, GPIO_PIN_5)){
-			GPIO_WritePin(GPIOA, GPIO_PIN_1, RESET);
-		}
-		else if(GPIO_ReadPin(GPIOA, GPIO_PIN_6)){
-			GPIO_WritePin(GPIOA, GPIO_PIN_1, RESET);
-		}
-		else if(GPIO_ReadPin(GPIOA, GPIO_PIN_7)){
-			GPIO_WritePin(GPIOA, GPIO_PIN_1, RESET);
-		}
-		else{
-			GPIO_WritePin(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, SET);
-		}
+		FND_DispDigit();
+		usleep(1000);
 	}
 
 	return 0;
