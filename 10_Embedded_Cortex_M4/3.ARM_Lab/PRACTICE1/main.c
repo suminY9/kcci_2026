575a16b1a6e7ee02f9da93d65daed6ad6b945266
unknown <sumin1201@ajou.ac.kr>
2026-05-28 15:51:38 +0900

    PARCTICE1_ver.1.1: modified timer interrupt timing

--- 10_Embedded_Cortex_M4/3.ARM_Lab/PRACTICE1/main.c
+++ 10_Embedded_Cortex_M4/3.ARM_Lab/PRACTICE1/main.c
@@ -14,9 +14,9 @@ volatile int Key_Pressed = 0;
 volatile int Uart_Data_In = 0;
 volatile unsigned char Uart_Data = 0;
 volatile int TIM2_Expired = 0;
-int cnt_1sec_led = 0;
-int cnt_50msec = 0;
-int cnt_1sec_uart = 0;
+int cnt_1sec_led = 1;
+int cnt_500msec = 1;
+int cnt_1sec_uart = 1;
 int uart_tag = 0;
 
 void Main(void)
@@ -33,12 +33,16 @@ void Main(void)
 		if(TIM2_Expired)
 	    {
 			// Internal LED Toggle
-			if(cnt_1sec_led/1000) cnt_1sec_led++;
-			else LED_INT_Toggle();
+			if(cnt_1sec_led%10) {
+				cnt_1sec_led++;
+			} else {
+				LED_INT_Toggle();
+				cnt_1sec_led = 1;
+			}
 
 			// External LED Toggle When Key pressed
 			if(Key_Pressed){
-				if(cnt_50msec/500) cnt_50msec++;
+				if(cnt_500msec%5) cnt_500msec++;
 				else{
 					if(Key_Pressed > 3){
 						Key_Pressed = 0;
@@ -47,20 +51,23 @@ void Main(void)
 						LED_EXT_Toggle();
 						Key_Pressed++;
 					}
+					cnt_500msec = 1;
 				}
 			}
 
 			// Send 'h', 'i' When 'a' Recieved
 			if(Uart_Data_In){
-				if(cnt_1sec_uart/1000) cnt_1sec_uart++;
-				else if(!uart_tag) {
+				if(!uart_tag) {
 					Uart2_Send_Byte('h');
 					uart_tag = 1;
+					cnt_1sec_uart = 1;
 				}
+				else if(cnt_1sec_uart%10) cnt_1sec_uart++;
 				else if(uart_tag) {
 					Uart2_Send_Byte('i');
 					uart_tag = 0;
 					Uart_Data_In = 0;
+					cnt_1sec_uart = 1;
 				}
 			}
 
