c4e87bf1828a43c836d71e11c78826daf340606c
unknown <sumin1201@ajou.ac.kr>
2026-07-02 15:30:13 +0900

    display up scaling (QVGA -> VGA)

--- 12_VGA/src/ImgRomReader.sv
+++ 12_VGA/src/ImgRomReader.sv
@@ -11,11 +11,14 @@ module ImgRomReader(
     output logic [3:0]  port_blue
 );
 
-    assign addr = de ? (320*y_pixel + x_pixel) : 'bz;  // de가 LOW일 때 출력 Z
-    // assign {port_red, port_green, port_blue} = de ? {imgPxlData[15:12], imgPxlData[10:7], imgPxlData[4:1]} : 0;
+    logic displayArea;
 
-    always_comb begin
-        if((x_pixel > 320) || (y_pixel > 240)) {port_red, port_green, port_blue} = 0;
-        else {port_red, port_green, port_blue} = de ? {imgPxlData[15:12], imgPxlData[10:7], imgPxlData[4:1]} : 0;
-    end
+    assign displayArea = (x_pixel < 320) && (y_pixel < 240);
+
+    // QVGA output
+    // assign addr = displayArea ? (320*y_pixel + x_pixel) : 'bz;
+    // VGA output (x2 up scaling)
+    assign addr = displayArea ? (320*(y_pixel/2) + (x_pixel/2)) : 'bz;
+
+    assign {port_red, port_green, port_blue} = displayArea ? {imgPxlData[15:12], imgPxlData[10:7], imgPxlData[4:1]} : 0;
 endmodule
