7f7a54fd8eaa937c4b8f5d89731a15fd1ac96e78
unknown <sumin1201@ajou.ac.kr>
2026-07-02 11:03:36 +0900

    add sw_ctrl: 0-RGB_SW, 1-ColorBox

--- 12_VGA/src/VGA_SW_top.sv
+++ 12_VGA/src/VGA_SW_top.sv
@@ -4,6 +4,7 @@ module VGA_SW_top(
     input  logic [3:0] sw_red,
     input  logic [3:0] sw_green,
     input  logic [3:0] sw_blue,
+    input  logic       sw_ctrl,
     output logic       h_sync,
     output logic       v_sync,
     output logic [3:0] port_red,
@@ -14,6 +15,8 @@ module VGA_SW_top(
     logic [9:0] x_pixel;
     logic [9:0] y_pixel;
     logic       de;
+    logic [3:0] rgb_sw_red, rgb_sw_green, rgb_sw_blue;
+    logic [3:0] rgb_cb_red, rgb_cb_green, rgb_cb_blue;
 
     VGA_Decoder U_VGA_Decode(
         .clk(clk),
@@ -24,23 +27,39 @@ module VGA_SW_top(
         .y_pixel(y_pixel),
         .de(de)
     ); 
-    // VGA_RGB_SW U_VGA_RGB_SW(
-    //     .sw_red(sw_red),
-    //     .sw_green(sw_green),
-    //     .sw_blue(sw_blue),
-    //     .de(de),
-    //     .x_pixel(x_pixel),
-    //     .y_pixel(y_pixel),
-    //     .port_red(port_red),
-    //     .port_green(port_green),
-    //     .port_blue(port_blue)
-    // );
+    VGA_RGB_SW U_VGA_RGB_SW(
+        .sw_red(sw_red),
+        .sw_green(sw_green),
+        .sw_blue(sw_blue),
+        .de(de),
+        .x_pixel(x_pixel),
+        .y_pixel(y_pixel),
+        .port_red(rgb_sw_red),
+        .port_green(rgb_sw_green),
+        .port_blue(rgb_sw_blue)
+    );
     VGA_ColorBox U_VGA_ColorBox(
         .de(de),
         .x_pixel(x_pixel),
         .y_pixel(y_pixel),
-        .port_red(port_red),
-        .port_green(port_green),
-        .port_blue(port_blue)
+        .port_red(rgb_cb_red),
+        .port_green(rgb_cb_green),
+        .port_blue(rgb_cb_blue)
     );
+    MUX_2X1 U_MUX_RGB(
+        .sw_ctrl(sw_ctrl),
+        .rgb_sw({rgb_sw_red, rgb_sw_green, rgb_sw_blue}),
+        .rgb_colorbox({rgb_cb_red, rgb_cb_green, rgb_cb_blue}),
+        .rgb_out({port_red, port_green, port_blue})
+    );
+endmodule
+
+
+module MUX_2X1(
+    input  logic        sw_ctrl,
+    input  logic [11:0] rgb_sw,
+    input  logic [11:0] rgb_colorbox,
+    output logic [11:0] rgb_out
+);
+    assign rgb_out = sw_ctrl? rgb_colorbox : rgb_sw;
 endmodule
--- 12_VGA/xdc/Basys-3-Master.xdc
+++ 12_VGA/xdc/Basys-3-Master.xdc
@@ -21,7 +21,7 @@ set_property -dict { PACKAGE_PIN V2    IOSTANDARD LVCMOS33 } [get_ports {sw_red[
 set_property -dict { PACKAGE_PIN T3    IOSTANDARD LVCMOS33 } [get_ports {sw_red[1]}]
 set_property -dict { PACKAGE_PIN T2    IOSTANDARD LVCMOS33 } [get_ports {sw_red[2]}]
 set_property -dict { PACKAGE_PIN R3    IOSTANDARD LVCMOS33 } [get_ports {sw_red[3]}]
-#set_property -dict { PACKAGE_PIN W2    IOSTANDARD LVCMOS33 } [get_ports {sw[12]}]
+set_property -dict { PACKAGE_PIN W2    IOSTANDARD LVCMOS33 } [get_ports {sw_ctrl}]
 #set_property -dict { PACKAGE_PIN U1    IOSTANDARD LVCMOS33 } [get_ports {sw[13]}]
 #set_property -dict { PACKAGE_PIN T1    IOSTANDARD LVCMOS33 } [get_ports {sw[14]}]
 #set_property -dict { PACKAGE_PIN R2    IOSTANDARD LVCMOS33 } [get_ports {sw[15]}]
